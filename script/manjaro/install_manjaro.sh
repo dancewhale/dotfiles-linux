48570e6d2ebb92298cc8465c0fbc575838137741
caoyuanzhi <542727233@qq.com>
2021-11-10 04:20:50 +0800

    Update config and script
    Emacs dotfiles update to latest.
    Update script for manjaro.

--- script/manjaro/install_manjaro.sh
+++ script/manjaro/install_manjaro.sh
@@ -14,17 +14,6 @@ EFO
 }
 
 
-basePackage() {
-  sudo pacman --noconfirm -Sy archlinuxcn-keyring yay
-
-  sudo pacman --noconfirm -Sy chezmoi  fzf  rofi  barrier docker
-
-  sudo pacman --noconfirm -Sy gvim  emacs=27.1-3
-
-  sudo pacman --noconfirm -Sy binutils  base-devel
-}
-
-
 bashSetting() {
   # configure for FZF.
   grep  "bash_my" ~/.bash_profile || sudo cat >> ~/.bash_profile <<EFO
@@ -39,34 +28,6 @@ EFO
 }
 
 
-chineseSetting() {
-  sudo pacman -R $(pacman -Qsq fcitx)
-  sudo pacman --noconfirm -S fcitx5-chinese-addons fcitx5 fcitx5-gtk  fcitx5-qt fcitx5-rime \
-	    fcitx5-configtool
-
-  # 其它文泉驿字体
-  sudo pacman --noconfirm -S wqy-microhei-lite  wqy-bitmapfont  wqy-zenhei
-  # 选用字体
-  sudo pacman --noconfirm -S adobe-source-han-sans-cn-fonts  adobe-source-han-serif-cn-fonts  noto-fonts-cjk
-  yay -S --noconfirm wqy-microhei rime-cloverpinyin && fc-cache -fv
- 
-  homeDir="Public  Templates Videos Pictures Documents Download Music Desktop"
-  for dir in $homeDir
-  do
-    if [ ! -d ~/$dir ]
-    then
-      mkdir ~/$dir
-    fi
-  done
-}
-
-
-syncSetting() {
-  sudo pacman --noconfirm -S synology-drive-client
-}
 
 pacmanset
-basePackage
 bashSetting
-chineseSetting
-syncSetting
--- script/manjaro/install_tools.sh
+++ script/manjaro/install_tools.sh
@@ -1,10 +1,43 @@
 #!/bin/bash -e
-sudo pacman --noconfirm -Sy cargo
+sudo pacman --noconfirm -Sy cargo gcc make
 
-which cargo || cargo install grex ripgrep sd
+which cargo && cargo install grex
 
-sudo pacman --noconfirm -S jq tokei  fd  bat exa zoxide ripgrep   ack  the_sliver_searcher
+sudo pacman --noconfirm -S jq tokei  fd  bat exa zoxide ripgrep   ack  the_silver_searcher
 
-sudo pacman --noconfirm -S ack  cht.sh cht.sh calibre
+sudo pacman --noconfirm -S ack  calibre
 
-sudo pacman --noconfirm -S visual-studio-code-bin
+sudo pacman --noconfirm -S code
+
+sudo pacman --noconfirm -Sy archlinuxcn-keyring yay
+
+sudo pacman --noconfirm -Sy chezmoi  fzf  rofi  barrier docker
+
+sudo pacman --noconfirm -Sy gvim  emacs  net-tools
+
+sudo pacman --noconfirm -Sy binutils  base-devel
+
+sudo pacman --noconfirm -S synology-drive-client
+
+chineseSetting() {
+  sudo pacman -R $(pacman -Qsq fcitx)
+  sudo pacman --noconfirm -S fcitx5-chinese-addons fcitx5 fcitx5-gtk  fcitx5-qt fcitx5-rime \
+	    fcitx5-configtool
+
+  # 其它文泉驿字体
+  sudo pacman --noconfirm -S wqy-microhei-lite  wqy-bitmapfont  wqy-zenhei
+  # 选用字体
+  sudo pacman --noconfirm -S adobe-source-han-sans-cn-fonts  adobe-source-han-serif-cn-fonts  noto-fonts-cjk
+  yay -S --noconfirm wqy-microhei rime-cloverpinyin && fc-cache -fv
+ 
+  homeDir="Public  Templates Videos Pictures Documents Download Music Desktop"
+  for dir in $homeDir
+  do
+    if [ ! -d ~/$dir ]
+    then
+      mkdir ~/$dir
+    fi
+  done
+}
+
+chineseSetting
