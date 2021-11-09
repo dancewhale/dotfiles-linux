81ba9332c2c6a0b5ae48cc759fd8a1bec52daf7f
caoyuanzhi <542727233@qq.com>
2021-11-10 04:31:54 +0800

    Remove old proxy network relative code.
    Migrate to surge proxy and not need this code.

--- dot_local/bin/executable_proxy-unset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-rm /tmp/.proxy-unset.sh||true
-sed    "s/-A/-D/g"  ~/.local/bin/proxy-set.sh > /tmp/.proxy-unset.sh
-sed -i "s/-N/-X/g"  /tmp/.proxy-unset.sh
-sed -i "s/-xe/-x/g" /tmp/.proxy-unset.sh
-sed -i '2{H;d}; ${p;x;s/^\n//}' /tmp/.proxy-unset.sh
-sed -i "s/start/stop/g"     /tmp/.proxy-unset.sh
-chmod +x /tmp/.proxy-unset.sh
-sudo /tmp/.proxy-unset.sh
