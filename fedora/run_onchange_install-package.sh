3933f8eb27eecb8c93d5a6d4777d911f800a705e
dancewhale <542727233@qq.com>
2023-03-01 23:09:04 +0800

    Setting git use vim and add package install script.

---
+++ fedora/run_onchange_install-package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+sudo yum install -y cargo gcc make
+
+which cargo && cargo install grex
+
+sudo yum install -y  jq tokei  bat exa zoxide ripgrep   ack  the_silver_searcher
+sudo yum install -y  ack fd-find  calibre
+
+# vscode  synology-drive-client
+
+sudo yum install -y  fzf  rofi  barrier docker
+
+sudo yum install -y  gvim  emacs  net-tools
+
+# install vscode
