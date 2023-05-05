14975904a69ef147336f9b42bd63eb286d9e0843
dancewhale <542727233@qq.com>
2023-05-05 11:08:56 +0800

    Update: auto seafile sync library.

--- fedora/run_onchange_after_install-package.sh
+++ fedora/run_onchange_after_install-package.sh
@@ -68,3 +68,6 @@ sudo dnf install -y seafile-client
 systemctl --user daemon-reload
 systemctl --user enable seafile
 systemctl --user start seafile
+
+seaf-cli sync -l "3440279a-fd36-4e94-bc0e-d3da402a1e58"  -s "{{ (bitwardenFields "item" "b27f7204-9341-4396-804e-aff9002a478a").url.value }}" -d ~/Dropbox  -u "{{ (bitwardenFields "item" "b27f7204-9341-4396-804e-aff9002a478a").user.value }}" -p  "{{ (bitwardenFields "item" "b27f7204-9341-4396-804e-aff9002a478a").password.value }}"
+
