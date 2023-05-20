da3c587f9003fb3c076c225e690a53ff5e41ab3e
dancewhale <542727233@qq.com>
2023-05-19 21:03:31 -0400

    Update: emacs add 2 theme package.

--- emacs/doom.d/modules/dancewhale/theme/packages.el
+++ emacs/doom.d/modules/dancewhale/theme/packages.el
@@ -1,2 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; dancewhale/theme/packages.el
+
+(package! jbeans-emacs
+  :recipe (:host github :repo "synic/jbeans-emacs" :branch "master"))
+
+(package! moe-theme.el
+  :recipe (:host github :repo "kuanyui/moe-theme.el" :branch "master"))
