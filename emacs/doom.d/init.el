5e6745776ac5cd56deeceef68d6dd19a14d158bc
dancewhale <542727233@qq.com>
2023-05-30 14:50:55 +0800

    Emacs: enable dap-mode in doom init

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -92,7 +92,7 @@
        :tools
        tree-sitter
        ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
@@ -102,7 +102,7 @@
        ;;gist              ; interacting with github gists
        (lookup                  ; helps you navigate your code and documentation
 	+docsets)               ; ...or in Dash docsets locally
-       (lsp)
+       (lsp +peek)
        (magit
 	+forge)
        make                     ; run make tasks from Emacs
