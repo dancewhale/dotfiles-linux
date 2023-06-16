75f0e3340d81956a8d646a6cd837a2a4d7edd26e
dancewhale <542727233@qq.com>
2023-06-16 17:41:35 +0800

    Emacs: remove helm from init.el fix yank error.

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -21,8 +21,9 @@
 
        :completion
        ;; company           ; the ultimate code completion backend
-       (helm
-	+childframe)             ; the *other* search engine for love and life
+       ;;(helm
+       ;;	+childframe)
+					; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy
 	+childframe)                              ; a search engine for love and life
