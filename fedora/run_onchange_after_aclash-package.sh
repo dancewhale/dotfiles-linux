a87d522204417411782a6b2bd5445c604c306f99
dancewhale <542727233@qq.com>
2023-06-18 11:45:19 +0800

    Fedora: clash init change and remove tripod.

--- fedora/run_onchange_after_aclash-package.sh
+++ fedora/run_onchange_after_aclash-package.sh
@@ -3,16 +3,20 @@ set -x
 # 创建专用账号 配置clash
 sudo useradd -M -s /usr/sbin/nologin clash
 
-# 首先初始化clash 
+# 首先初始化clash
 sudo cp ~/.config/clash/clash.service /usr/lib/systemd/system/clash.service
 
 sudo mkdir /etc/clash
 sudo cp -r ~/.config/clash/* /etc/clash
-sudo chown -R clash:clash /etc/clash
+
+sudo wget http://67.230.186.218/clash-linux-${ARCH}-2023.04.16 -O /usr/local/bin/clash
+
+sudo chmod +x /usr/local/bin/clash
+
+sudo chown -R clash:clash /etc/clash /usr/local/bin/clash
 
 sudo systemctl daemon-reload
 
 sudo systemctl enable clash.service
 
 sudo systemctl start clash.service
-
--- init.sh
+++ init.sh
@@ -10,8 +10,6 @@ fi
 
 sudo wget http://67.230.186.218/chezmoi_2.33.3_linux_${ARCH}/chezmoi -P /usr/local/bin/
 
-sudo wget http://67.230.186.218/clash-linux-${ARCH}-2023.04.16 -O /usr/local/bin/clash
-
 sudo chmod +x /usr/local/bin/*
 
 chezmoi init
