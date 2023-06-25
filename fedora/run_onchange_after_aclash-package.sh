83c248dce0d9fc8b0c426bef61e755fa6f758774
dancewhale <542727233@qq.com>
2023-06-25 15:00:27 +0800

    Fedora: fix install error in 36 OS version.

--- fedora/run_onchange_after_aclash-package.sh
+++ fedora/run_onchange_after_aclash-package.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 set -x
+
+ARCH=$(uname -m)
+
+if [ $ARCH = "aarch64" ]; then
+        export ARCH=arm64
+elif [ $ARCH = "x86_64" ]; then
+        export ARCH=amd64
+fi
+
 # 创建专用账号 配置clash
 sudo useradd -M -s /usr/sbin/nologin clash
 
@@ -9,7 +18,7 @@ sudo cp ~/.config/clash/clash.service /usr/lib/systemd/system/clash.service
 sudo mkdir /etc/clash
 sudo cp -r ~/.config/clash/* /etc/clash
 
-sudo wget http://67.230.186.218/clash-linux-${ARCH}-2023.04.16 -O /usr/local/bin/clash
+ls /usr/local/bin/clash  || sudo wget http://67.230.186.218/clash-linux-${ARCH}-2023.04.16 -O /usr/local/bin/clash
 
 sudo chmod +x /usr/local/bin/clash
 
