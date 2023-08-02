3aaf41f5e3eaa32a3056aaca1f9154262af287c1
caoyuanzhi <542727233@qq.com>
2023-08-02 15:35:45 +0800

    ARCH: fix init script.

--- arch/script/clash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-paru -Sy clash-meta-bin
-
-sudo cp -r ~/.config/clash/*  /etc/clash-meta/
--- arch/script/container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# 安装k8s 相关
-
-sudo pacman -S docker kubectl
-
-groups | grep docker || sudo gpasswd -a $USER docker
