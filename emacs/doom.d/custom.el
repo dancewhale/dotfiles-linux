cc46d0734fdb7fd3b5c8b766a272f6ae72fc6a25
dancewhale <542727233@qq.com>
2023-08-22 09:51:40 +0800

    Emacs: fix org-date color to gray.

--- emacs/doom.d/custom.el
+++ emacs/doom.d/custom.el
@@ -56,4 +56,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-date ((t (:foreground "dim gray"))))
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
