189369204b62e45bbecd577cc6d246ebd1cd5d27
dancewhale <542727233@qq.com>
2025-06-04 09:45:01 +0800

    Fix emacsclient script error.

--- endeavouros/dot_config/hypr/UserScripts/executable_EmacsClient.sh
+++ endeavouros/dot_config/hypr/UserScripts/executable_EmacsClient.sh
@@ -7,15 +7,24 @@ window_exists() {
 
 # 创建 EmacsDrop 窗口
 create_drop_window() {
-    emacsclient -r --frame-parameters='(quote (name . "EmacsDrop"))'
+    emacsclient -c --frame-parameters='(quote (name . "EmacsDrop"))' -e '(cyz-display-init)'
 }
 
 # 创建 EmacsSystem 窗口并打开文件
 create_system_window() {
     local num="$1"
     local filepath="$2"
-    emacsclient -r --frame-parameters='(quote (name . "EmacsSystem"))' -n -a "" "+$num" "$filepath"
+    emacsclient -c --frame-parameters='(quote (name . "EmacsSystem"))' -n  "+$num" "$filepath"
 }
+
+# 在EmacsSystem 窗口中打开文件
+open_system_window() {
+    local num="$1"
+    local filepath="$2"
+    emacsclient -n "+$num" "$filepath"
+}
+
+
 
 # 切换到 EmacsSystem 窗口
 focus_system_window() {
@@ -33,9 +42,12 @@ if [[ "$1" == "--drop" ]]; then
 elif [[ "$1" == "--file" && -n "$2" && "$3" == "--num" && -n "$4" ]]; then
     filepath="$2"
     num="$4"
-    #if ! window_exists; then
+    if ! window_exists; then
         create_system_window "$num" "$filepath"
-    #fi
+    fi
+    if window_exists; then
+        open_system_window "$num" "$filepath"
+    fi
     focus_system_window
 else
     echo "用法:"
