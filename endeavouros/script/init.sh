d9c194944199489199add8eb5aaefd208aacd4c1
dancewhale <542727233@qq.com>
2025-06-07 09:57:31 +0800

    Fix init script: remove Expired Codes and add nvim package.

--- endeavouros/script/init.sh
+++ endeavouros/script/init.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 
 main() {
-    init-constants
     update-system
     install-paru
     install-packages
     install-go-packages
     set-user-groups
     enable-services
-    set-leftovers
     post-install-message
-}
-
-init-constants() {
-    FX_RESET="\033[0m"
-    FX_BOLD="\033[1m"
-    FX_ITALIC="\033[3m"
-
-    FG_RED="\033[31m"
-    FG_GREEN="\033[32m"
-    FG_YELLOW="\033[33m"
-    FG_CYAN="\033[36m"
-    FG_WHITE="\033[37m"
-
-    BG_MAGENTA="\033[45m"
-
-    # ERROR
-    #printf '%b%s%b\n' "${FX_BOLD}${FG_RED}"
-    # WARN
-    #printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}"
-    # SUCESSFUL
-    #printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}"
-    # INFO
-    #printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}"
+    post-install-nvim
 }
 
 update-system() {
@@ -212,14 +188,14 @@ install-go-packages() {
 
 enable-services() {
     local services=(
-        sddm
-        docker
-        clash-meta
     )
 
     local users=(
         waynergy
+	emacs
         seafile
+	kmonad
+	pypr
     )
 
     # Enable services if they exist and are not enabled
@@ -258,104 +234,6 @@ enable-services() {
     #playerctld daemon                   # if it doesn't work try installing volumectl
 }
 
-set-leftovers() {
-    # Disable the systemd-boot startup entry if systemd-boot is installed
-    if [ -d "/sys/firmware/efi/efivars" ] && [ -d "/boot/loader" ]; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Disabling systemd-boot startup entry"
-        sudo sed -i 's/^timeout/# timeout/' /boot/loader/loader.conf
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Disabled systemd-boot startup entry"
-    else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "systemd-boot is not being used."
-    fi
-
-    # Set data locale english if it is not set
-    if [[ "$(localectl status)" != *"LC_TIME=en_US.UTF8"* ]]; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Setting LC_TIME to English..."
-        sudo localectl set-locale LC_TIME=en_US.UTF8
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "LC_TIME set to English."
-    else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "LC_TIME is already set to English."
-    fi
-
-    # Install GRUB theme if GRUB is installed
-    if command -v grub-install >/dev/null && ! command -v bootctl >/dev/null; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Installing GRUB theme..."
-
-        # Clone theme repository
-        git clone https://github.com/HenriqueLopes42/themeGrub.CyberEXS
-        mv themeGrub.CyberEXS CyberEXS
-
-        # Move theme directory to GRUB themes directory
-        sudo mv CyberEXS /boot/grub/themes/
-
-        # Update GRUB configuration
-        sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-        # Set GRUB theme in GRUB configuration
-        echo 'GRUB_THEME=/boot/grub/themes/CyberEXS/theme.txt' | sudo tee -a /etc/default/grub
-
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "GRUB theme installed."
-    else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "GRUB bootloader is not installed, using systemd-boot."
-    fi
-
-    # Create Hyprland desktop entry if Hyprland is installed
-    if command -v Hyprland >/dev/null; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Creating Hyprland desktop entry..."
-
-        sudo bash -c "cat <<EOF > /usr/share/wayland-sessions/hyprland.desktop
-[Desktop Entry]
-Name=Hyprland
-Comment=hyprland
-Exec=bash -c \"$HOME/.config/hypr/scripts/starth\"
-Type=Application
-"
-
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Hyprland desktop entry created."
-    else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_RED}" "Hyprland is not installed."
-    fi
-
-    # SDDM rice (don't install GDM cuz it installs GNOME DE as dependency)
-    if command -v sddm >/dev/null; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Creating /etc/sddm.conf file..."
-
-        sudo bash -c 'cat <<EOF > /etc/sddm.conf
-# Use autologin if have problems with sddm
-#[Autologin]
-#Session=hyprland
-#User=twilight
-
-[Theme]
-Current=astronaut
-CursorSize=24
-CursorTheme=Numix-Cursor-Light
-Font=JetBrains Mono
-ThemeDir=/usr/share/sddm/themes
-'
-
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "/etc/sddm.conf file created."
-    else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_RED}" "SDDM is not installed."
-    fi
-
-    # Define the desired button layout value (remove buttons - none)
-    desired_button_layout=":"
-
-    # Get the current button layout value using gsettings
-    current_button_layout=$(gsettings get org.gnome.desktop.wm.preferences button-layout)
-
-    # Compare the current value with the desired value using an if statement
-    if [ "$current_button_layout" != "$desired_button_layout" ]; then
-        # If they don't match, update the button layout using the gsettings command
-        gsettings set org.gnome.desktop.wm.preferences button-layout "$desired_button_layout"
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Button layout has been updated."
-    else
-        # If they match, display a message indicating that the value is already as desired
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "Button layout is already set as desired."
-    fi
-}
-
 post-install-message() {
     printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Post-Installation:"
     printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Start setting seafile."
@@ -372,6 +250,11 @@ post-install-message() {
 
     seaf-cli status | grep Dropbox || seaf-cli sync -l "838a54d5-3992-4a1e-9948-67d5c4d1a903" -s $surl -d ~/Dropbox -u $suser -p $spass
 
+}
+
+post-install-nvim() {
+    echo "Use :PackerInstall in nvim to install plugin\n"
+    echo "Use :PackerSync in nvim to sync plugin"
 }
 
 main "$@"
