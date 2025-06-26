c8b4b692d95da175415da45b5a58c170ef21a067
dancewhale <542727233@qq.com>
2025-06-26 18:55:22 +0800

    Add arch font install script.

---
+++ endeavouros/script/font.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo pacman -Sy ttf-iosevka-nerd
+sudo pacman -Sy ttf-firacode-nerd
+sudo pacman -Sy ttf-mononoki-nerd
+sudo pacman -Sy ttf-jetbrains-mono
+sudo pacman -Sy ttf-jetbrains-mono-nerd
+sudo pacman -Sy ttf-fantasque-sans-mono
+yay -Sy ttf-lxgw-wenkai-screen
+yay -Sy ttf-lxgw-wenkai-tc
+yay -Sy ttf-lxgw-wenkai-tc-mono
+yay -Sy ttf-harmonyos-sans
+yay -Sy nerd-fonts-sarasa-term
