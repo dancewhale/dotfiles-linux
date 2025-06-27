98f3365023da9a022e6413c6dee2b27424bd467f
dancewhale <542727233@qq.com>
2025-06-27 10:54:24 +0800

    Update font script and emacs conf.

--- endeavouros/script/font.sh
+++ endeavouros/script/font.sh
@@ -5,8 +5,7 @@ sudo pacman -Sy ttf-mononoki-nerd
 sudo pacman -Sy ttf-jetbrains-mono
 sudo pacman -Sy ttf-jetbrains-mono-nerd
 sudo pacman -Sy ttf-fantasque-sans-mono
+yay -Sy ttf-lxgw-bright-code-git
 yay -Sy ttf-lxgw-wenkai-screen
-yay -Sy ttf-lxgw-wenkai-tc
-yay -Sy ttf-lxgw-wenkai-tc-mono
 yay -Sy ttf-harmonyos-sans
 yay -Sy nerd-fonts-sarasa-term
