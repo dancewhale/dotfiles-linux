0acf4f1858266116b97174c8bdac2e5c78fa8d32
dancewhale <542727233@qq.com>
2025-07-08 15:08:27 +0800

    Update config.

---
+++ endeavouros/script/python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+paru -Sy python-mcp python-google-genai   --mflags "--nocheck"
+paru -Sy python-google-ai-generativelanguage --mflags "--nocheck"
+paru -Sy python-google-ai-generativelanguage --mflags "--nocheck"
+paru -Sy python-posthog --mflags "--nocheck"
